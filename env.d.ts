90bc61362f4400b413a3ff38149db6dfc2972aaf
lokism890-del <lokism890@gmail.com>
2026-09-12 01:18:44 +0500

    integration

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    SUPABASE_SERVICE_ROLE_KEY: string;
+    NEXT_PUBLIC_PADDLE_CLIENT_TOKEN: string;
+    NEXT_PUBLIC_PADDLE_PRICE_ID: string;
+    NEXT_PUBLIC_PADDLE_ENV: string;
+    PADDLE_API_KEY: string;
+    PADDLE_WEBHOOK_SECRET_KEY: string;
+  }
+}
